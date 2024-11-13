50323c78a15afe27cbbc47c9ea60f7f4d815209d
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-13 20:57:03 +0900

    chore: serialization の依存関係を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
     alias(libs.plugins.compose.compiler)
 }
 
