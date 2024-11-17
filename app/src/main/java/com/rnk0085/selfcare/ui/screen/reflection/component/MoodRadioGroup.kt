dad99b70e2c5f42ef64eb06fdbf4b70d49f91447
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-17 22:15:32 +0900

    chore: 不要なimportを削除

--- app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/component/MoodRadioGroup.kt
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/component/MoodRadioGroup.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,7 +18,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
