e5da3feda9d6239fc098b719f39f3f297f996327
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-17 20:08:29 +0900

    feat: DiaryDatePicker の大枠を作成

---
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/component/DiaryDatePicker.kt
@@ -0,0 +1,71 @@
+package com.rnk0085.selfcare.ui.screen.reflection.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+@Composable
+internal fun DiaryDatePicker(
+    modifier: Modifier = Modifier,
+) {
+    var selectedDate by remember { mutableStateOf(System.currentTimeMillis()) }
+
+    OutlinedButton(
+        onClick = { /* TODO */ },
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                imageVector = Icons.Default.DateRange,
+                contentDescription = null,
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = convertMillisToDate(selectedDate),
+                style = MaterialTheme.typography.labelMedium,
+            )
+        }
+    }
+}
+
+private fun convertMillisToDate(millis: Long): String {
+    val formater = SimpleDateFormat("yyyy/MM/dd(EEE)", Locale.getDefault())
+    return formater.format(Date(millis))
+}
+
+@Composable
+@Preview
+private fun DiaryDatePickerPreview() {
+    DiaryDatePicker()
+}
