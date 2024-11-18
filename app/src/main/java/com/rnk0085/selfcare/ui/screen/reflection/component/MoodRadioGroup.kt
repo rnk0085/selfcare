8d9c66b1e08e056df8ef27b2536369c3e7325c93
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-18 23:42:04 +0900

    fix: Stringリソースに追加

--- app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/component/MoodRadioGroup.kt
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/component/MoodRadioGroup.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,8 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.rnk0085.selfcare.R
 import com.rnk0085.selfcare.ui.theme.SelfcareTheme
 import com.rnk0085.selfcare.ui.theme.Spacing
@@ -40,7 +39,7 @@ internal fun MoodRadioGroup(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
        Text(
-           text = "どのような1日でしたか？",
+           text = stringResource(R.string.mood_selector_label),
            style = MaterialTheme.typography.labelMedium,
        )
 
