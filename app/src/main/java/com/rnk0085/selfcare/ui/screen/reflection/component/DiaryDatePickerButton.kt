73adb12a28b76ff59cb643a92f670b1db90e61da
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-18 23:03:40 +0900

    fix: カラーなどを修正（一旦の仮置き）

--- app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/component/DiaryDatePickerButton.kt
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/component/DiaryDatePickerButton.kt
@@ -5,20 +5,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.rnk0085.selfcare.ui.theme.sectionContainerColor
+import com.rnk0085.selfcare.ui.theme.sectionContentColor
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -29,12 +29,12 @@ internal fun DiaryDatePickerButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    OutlinedButton(
+    Button(
         onClick = onClick,
-        shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.outlinedButtonColors(
-            contentColor = Color.Black,
-            containerColor = Color.White,
+        shape = MaterialTheme.shapes.small,
+        colors = ButtonDefaults.buttonColors(
+            contentColor = sectionContentColor,
+            containerColor = sectionContainerColor,
         ),
         modifier = modifier.fillMaxWidth(),
     ) {
