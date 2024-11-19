8b9de2b9af44b7728576f75939d8d090c0f2ac82
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-19 20:37:28 +0900

    feat: 入力欄がキーボードで隠れないようにした

--- app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/ReflectionScreen.kt
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/ReflectionScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -51,6 +53,7 @@ internal fun ReflectionScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(innerPadding)
                 .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.SpaceEvenly,
