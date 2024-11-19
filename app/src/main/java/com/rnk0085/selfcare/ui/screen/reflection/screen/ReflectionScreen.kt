8ef063a8ad3897a22700c4762ebd9b67086719d7
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-19 22:51:27 +0900

    fix: ふりかえりの文言リソースを作成

--- app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/screen/ReflectionScreen.kt
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/screen/ReflectionScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.rnk0085.selfcare.R
 import com.rnk0085.selfcare.ui.screen.component.NavigationType
 import com.rnk0085.selfcare.ui.screen.component.SelfcareTopAppBar
 import com.rnk0085.selfcare.ui.screen.reflection.page.ReflectionPage
@@ -17,7 +19,7 @@ internal fun ReflectionScreen(
     Scaffold(
         topBar = {
             SelfcareTopAppBar(
-                title = "ふりかえり",
+                title = stringResource(R.string.reflection_top_bar_title),
                 navigationType = NavigationType.Back(onClick = onBackClicked),
             )
         },
