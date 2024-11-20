d97f51fd8a95c106425ca527a9465a1119de301b
rnk0085 <rinawkmbi4k8@gmail.com>
2024-11-20 16:49:36 +0900

    feat: 日付と気分を選択できるようにする

--- app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/ReflectionViewModel.kt
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/ReflectionViewModel.kt
@@ -1,14 +1,50 @@
 package com.rnk0085.selfcare.ui.screen.reflection
 
 import androidx.lifecycle.ViewModel
+import com.rnk0085.selfcare.ui.currentTimeMillis
+import com.rnk0085.selfcare.ui.screen.reflection.section.moodSelector.MoodType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 internal class ReflectionViewModel @Inject constructor() : ViewModel() {
     private val _uiState = MutableStateFlow(ReflectionUiState.InitialValue)
     val uiState: StateFlow<ReflectionUiState> = _uiState.asStateFlow()
+
+    fun onDateSelected(date: Long?) {
+        _uiState.update {
+            it.copy(
+                selectedDate = date ?: currentTimeMillis,
+                showDatePicker = false,
+            )
+        }
+    }
+
+    fun onMoodSelected(mood: MoodType) {
+        _uiState.update {
+            it.copy(
+                selectedMood = mood,
+            )
+        }
+    }
+
+    fun openDatePicker() {
+        _uiState.update {
+            it.copy(
+                showDatePicker = true,
+            )
+        }
+    }
+
+    fun closeDatePicker() {
+        _uiState.update {
+            it.copy(
+                showDatePicker = false,
+            )
+        }
+    }
 }
--- app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/screen/ReflectionScreen.kt
+++ app/src/main/java/com/rnk0085/selfcare/ui/screen/reflection/screen/ReflectionScreen.kt
@@ -5,17 +5,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.rnk0085.selfcare.R
-import com.rnk0085.selfcare.ui.currentTimeMillis
 import com.rnk0085.selfcare.ui.screen.component.NavigationType
 import com.rnk0085.selfcare.ui.screen.component.SelfcareTopAppBar
 import com.rnk0085.selfcare.ui.screen.reflection.ReflectionUiState
@@ -32,6 +27,10 @@ internal fun ReflectionScreen(
 
     ReflectionScreen(
         uiState = uiState,
+        openDatePicker = viewModel::openDatePicker,
+        closeDatePicker = viewModel::closeDatePicker,
+        onDateSelected = viewModel::onDateSelected,
+        onMoodSelected = viewModel::onMoodSelected,
         onBackClicked = onBackClicked,
     )
 }
@@ -40,16 +39,11 @@ internal fun ReflectionScreen(
 private fun ReflectionScreen(
     uiState: ReflectionUiState,
     onBackClicked: () -> Unit,
+    openDatePicker: () -> Unit,
+    closeDatePicker: () -> Unit,
+    onDateSelected: (Long?) -> Unit,
+    onMoodSelected: (MoodType) -> Unit,
 ) {
-    var selectedDate by remember { mutableLongStateOf(currentTimeMillis) }
-    var showDatePicker by remember { mutableStateOf(false) }
-
-    var selectedMood by remember { mutableStateOf<MoodType?>(null) }
-
-    var firstText by remember { mutableStateOf("") }
-    var secondText by remember { mutableStateOf("") }
-    var thirdText by remember { mutableStateOf("") }
-
     Scaffold(
         topBar = {
             SelfcareTopAppBar(
@@ -68,16 +62,13 @@ private fun ReflectionScreen(
             firstText = uiState.firstText,
             secondText = uiState.secondText,
             thirdText = uiState.thirdText,
-            openDatePicker = { showDatePicker = true },
-            closeDatePicker = { showDatePicker = false },
-            onDateSelected = {
-                selectedDate = it ?: currentTimeMillis
-                showDatePicker = false
-            },
-            onMoodSelected = { selectedMood = it },
-            onFirstTextChange = { firstText = it },
-            onSecondTextChange = { secondText = it },
-            onThirdTextChange = { thirdText = it },
+            openDatePicker = openDatePicker,
+            closeDatePicker = closeDatePicker,
+            onDateSelected = onDateSelected,
+            onMoodSelected = onMoodSelected,
+            onFirstTextChange = {},
+            onSecondTextChange = {},
+            onThirdTextChange = {},
             onRecordClick = { /** TODO **/ },
         )
     }
@@ -88,6 +79,10 @@ private fun ReflectionScreen(
 private fun ReflectionScreenPreview() {
     ReflectionScreen(
         uiState = ReflectionUiState.InitialValue,
+        openDatePicker = {},
+        closeDatePicker = {},
+        onDateSelected = {},
+        onMoodSelected = {},
         onBackClicked = {},
     )
 }
